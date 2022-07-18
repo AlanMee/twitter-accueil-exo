e2184e80b6588e9c921eb947316d102ce64fb4e4
AlanMee <101573653+AlanMee@users.noreply.github.com>
2022-07-18 14:28:16 +0200

    files add

---
+++ twitter/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Twitter </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+    <div class="contenu">
+
+        <div class="gauche"> <img src="images/logo-twitter-blanc.png"> </div>
+
+        <div class="droite">
+            <div class="logo"> <img src="images/logo-twitter-blanc.png"> </div> 
+            <div class="titre"> <h2> ça se passe </br> maintenant </h2> </div>
+            <div class="titre2"> <h3> Rejoignez Twitter dès aujourd'hui</h3> </div> 
+            <a class="google" href="#"> <img src="images/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"> <p> S'inscrire avec Google </p> </a>
+            <a class="apple" href="#"> <img src="images/580b57fcd9996e24bc43c516"> <p>  S'inscrire avec Apple </p> </a> 
+            <div class="ou"> <p> ou </p> </div>
+            <div class="tel"> <p> <a href="#"> S'inscire avec un numéro de téléphone </a> </p>  </div>
+            <div class="conditions"> <p> En vous inscrivant, vous acceptez les <a href="#">Conditions d'Utilisation</a> </br>
+            et la <a href="#">Politique de Confidentialité</a>, 
+            incluant l'<a href="#">Utilisation de </br> Cookies.</a> </p> </div>
+            <div class="deja"> <h4> Vous avez déjà un compte? </h4> </div> 
+            <div class="connecter"> <p> <a href="#"> Se connecter </a> </p> </div>
+            
+        </div> 
+
+    </div>
+
+</body>
+
+<footer> 
+    <div class= "contenu-footer">
+        <p> 
+            <a href="#">À propos </a>  
+            <a href="#">Centre d'assistance </a>
+            <a href="#">Conditions d’utilisation </a>
+            <a href="#">Politique de Confidentialité </a>
+            <a href="#">Politique relative aux cookies </a> 
+            <a href="#">Accessibilité Informations sur les publicités </a> 
+            <a href="#">Blog </a>
+            <a href="#">Statut Carrières </a>
+            <a href="#">Ressources de la marque </a>
+            <a href="#">Publicité </a>
+            <a href="#">Marketing </a>
+            <a href="#">Twitter pour les professionnels </a>
+            <a href="#">Développeurs </a>
+            <a href="#">Répertoire </a>
+            </br>
+            <a href="#">Paramètres </a>
+            © 2022 Twitter, Inc. 
+        </p> 
+    </div> 
+
+
+
+</footer>
+
+</html>
